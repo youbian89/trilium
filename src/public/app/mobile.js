9678e6656f345e55441fdaf0866b383a57936612
youbian89 <48555962+youbian89@users.noreply.github.com>
2022-11-05 21:23:13 +0800

    Update mobile.js
    修改移动端布局为v2，符合我使用习惯的UI

--- src/public/app/mobile.js
+++ src/public/app/mobile.js
@@ -1,5 +1,5 @@
 import appContext from "./services/app_context.js";
-import MobileLayout from "./layouts/mobile_layout_v2.js";
+import MobileLayout_v2 from "./layouts/mobile_layout_v2.js";
 import glob from "./services/glob.js";
 
 glob.setupGlobs();
