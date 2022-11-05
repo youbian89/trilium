c72f240156ed1df76f7311e90ea8f4e53c64327a
youbian89 <48555962+youbian89@users.noreply.github.com>
2022-11-05 21:06:27 +0800

    Update mobile.js

--- src/public/app/mobile.js
+++ src/public/app/mobile.js
@@ -1,8 +1,8 @@
 import appContext from "./services/app_context.js";
-import MobileLayout from "./layouts/mobile_layout.js";
+import MobileLayout from "./layouts/mobile_layout_v2.js";
 import glob from "./services/glob.js";
 
 glob.setupGlobs();
 
-appContext.setLayout(new MobileLayout());
+appContext.setLayout(new MobileLayout_v2());
 appContext.start();
